ad66e584ba717cc3dfc720d79e34b2d3f55ad02c
paularah <p.arah@alustudent.com>
2020-04-10 10:52:01 +0100

    welcome

--- week9/question3.js
+++ week9/question3.js
@@ -25,46 +25,49 @@ class MinBinaryHeap{
     }
 
     extractMin(){
-    if (this.values.length <= 1) return null;
-     let min = this.values[0], 
-         lastVal = this.values.pop();
-    this.values[0] = lastVal;
-
-     return min;
+        let min = this.values[0]
+        let lastVal = this.values.pop();
+        if (this.values.length > 1){
+        this.values[0] = lastVal;
+        this.heapifyDown();
+        }
+        return min;
     }
 
     heapifyDown(){
         let idx = 0,
             element = this.values[idx],
-            swap = null,
+            swap,
             leftChildIdx,
             rightChildIdx,
             leftChild,
             rightChild;
         while (true){
+            swap = null;
             leftChildIdx = (2 * idx) + 1;
             rightChildIdx = (2 * idx) + 2;
             leftChild = leftChildIdx < this.values.length ? this.values[leftChildIdx] : null;
-            rightChild = rightChildIdx < this.values.length ? this.values[leftChildIdx] : null;
-            swap = leftChild < rightChild ? leftChildIdx : rightChildIdx; 
-            if (!swap) break;
-            if (swap < element){
+            rightChild = rightChildIdx < this.values.length ? this.values[rightChildIdx] : null;
+            if (leftChild <= rightChild && leftChild < element && leftChild !== null){
+                swap = leftChildIdx
+            }
+            if (leftChild >= rightChild && rightChild < element && rightChild !== null){
+                swap = rightChildIdx;
+            }
+            if (swap === null) break;
             this.values[idx] = this.values[swap];
             this.values[swap] = element;
-            idx = swap;
-            }
-            
+            idx = swap;          
     }
 }
 }
 
 let heap = new MinBinaryHeap();
-
 heap.insert(1)
 heap.insert(4)
 heap.insert(2)
 heap.insert(9)
 heap.insert(3)
 heap.insert(15)
-
+heap.extractMin();
 console.log(heap.values)
